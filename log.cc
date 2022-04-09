af871ff92483344eae899e8758e533b8d3c69aa4
Valyria <93093634+rocvalyria@users.noreply.github.com>
2022-04-09 12:51:01 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,5 +1,5 @@
  
  Welcome to YamatosLoader.
  
--(9/4/2022) Loader Crash Issue Fixed. 
-Download Instructions Again.
+-(9/4/2022) Loader Crash Issue Fixed. Download Instructions Again.
+-(9/4/2022)

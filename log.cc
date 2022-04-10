23b524aab7c1f0997a09940eb040127a5c502666
Valyria <93093634+rocvalyria@users.noreply.github.com>
2022-04-10 23:51:45 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,6 +1,10 @@
  
  Welcome to YamatosLoader.
  
+- (Authentication failed. => Wrong Hwid Need Hwid Reset)
+- (Something went wrong with driver. => Driver Load Error. Something Blocked Driver on PC.)
+- (Your key is error or used. => Key.dat File Missing.)
+
 -(9/4/2022) Loader Crash Issue Fixed. Download Instructions Again.
 -(9/4/2022) Loader Auto Updated.
--(Note: Never delete "key.dat" file. Everytime save!)
+-(10/4/2022) Spoofer will be release in few days.

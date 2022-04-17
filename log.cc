a561ea4672dca582e90a7691c0e21bd176dcb8b9
Valyria <93093634+rocvalyria@users.noreply.github.com>
2022-04-17 22:14:34 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -6,7 +6,4 @@
 - (Something went wrong with driver. => Driver Load Error. Something Blocked Driver on PC.)
 - (Your key is error or used. => Key.dat File Missing.)
 
--(9/4/2022) Loader Crash Issue Fixed. Download Instructions Again.
--(9/4/2022) Loader Auto Updated.
--(10/4/2022) Spoofer will be release in few days.
--(12/4/2022) Aimbot: BAN RISK.
+-(17/4/2022) Cheat Updated. Spoofer Updated.

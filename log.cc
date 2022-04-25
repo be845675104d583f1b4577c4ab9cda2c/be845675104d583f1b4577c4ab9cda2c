c60c2945423a41967a482a2ff746b8403068e221
Valyria <93093634+rocvalyria@users.noreply.github.com>
2022-04-25 04:10:44 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,12 +1,9 @@
  
  Welcome to YamatosLoader.
- STATUS: Undetected
+ STATUS: Risk
  
 - (Authentication failed. => Wrong Hwid Need Hwid Reset)
 - (Something went wrong with driver. => Driver Load Error. Something Blocked Driver on PC.)
 - (Your key is error or used. => Key.dat File Missing.)
 
--(17/4/2022) Cheat Updated. Spoofer Updated.
--(18/4/2022) Loader Instant Close Fixed.
--(23/4/2022) Tested. Still Undetected.
--(24/4/2022) Cheat Work Perfect. | Spoofer Have Some Issues.
+-(25/4/2022) Status Changed. We are working on it dont worry :)

6166de79aeda310cbecebc98e82e242bae140d89
Valyria <93093634+rocvalyria@users.noreply.github.com>
2022-04-27 01:17:58 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,9 +1,9 @@
  
  Welcome to YamatosLoader.
- STATUS: Risk
+ STATUS: UNDETECTED
  
 - (Authentication failed. => Wrong Hwid Need Hwid Reset)
 - (Something went wrong with driver. => Driver Load Error. Something Blocked Driver on PC.)
 - (Your key is error or used. => Key.dat File Missing.)
 
--(25/4/2022) Status Changed. We are working on it dont worry :)
+-(27/4/2022) Updated. Working Fine. UNDETECTED!

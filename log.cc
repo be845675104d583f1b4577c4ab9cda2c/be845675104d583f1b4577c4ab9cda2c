2026f96e2fa5e1c4f8f2cd5ca35f4f1a766068f3
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-05-19 22:35:33 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -7,5 +7,3 @@
 - (Your key is error or used. => Key.dat File Missing.)
 
 -(30/04/2022) Misc -> Field of View added. (New Feature)
--(08/04/2022) Loader -> Auto Updated.
--(08/04/2022) Spoofer -> Thread4 Stuck Fixed.

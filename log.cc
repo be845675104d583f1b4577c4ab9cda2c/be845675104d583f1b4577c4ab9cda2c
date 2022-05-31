3c006fda420b8efb420b8b9b66dd4fc2f26a19ec
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-05-31 08:42:49 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,8 +1,4 @@
  Welcome to YamatosLoader.
  STATUS: UNDETECTED
  
-- (Authentication failed. => Wrong Hwid Need Hwid Reset)
-- (Something went wrong with driver. => Driver Load Error. Something Blocked Driver on PC.)
-- (Your key is error or used. => Key.dat File Missing.)
-
 -(30/04/2022) Misc -> Field of View added. (New Feature)

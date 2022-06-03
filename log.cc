d5309ec4d7531401ba60ef02b49a87624e53f827
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-06-03 06:02:33 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -3,6 +3,6 @@
  
 -(30/04/2022) Misc -> Field of View added. (New Feature)
  
-    - We are working on Error 0xD001. Maintenance will be over in 1-2 hours.
+    - We are working on Error 0xD004. Maintenance will be over in 1-2 hours.
   
   p0001

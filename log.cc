3474bf16f7b13dc0254e4fadc135ecacbd657a9f
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-06-05 16:40:03 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,3 +1,3 @@
-Motherfuckers who send nonsense logs, requests (like hwid reset, etc.) instead of error code; float kids, float!
+Motherfuckers who send nonsense logs, requests (like hwid reset, etc.) instead of error code; fly kids!
 • YamatosLoader is under maintenance for a few hours.
 p0001

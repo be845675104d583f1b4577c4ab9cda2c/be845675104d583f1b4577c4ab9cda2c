fade3dff86f5efebe5d8c09ff45995fe0743a2d5
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-06-05 16:43:42 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,3 +1,3 @@
 Motherfuckers who send nonsense logs, requests (like hwid reset, etc.) instead of error code; fly with banned license!
-• YamatosLoader is under maintenance for a few hours.
+-> YamatosLoader is under maintenance for a few hours.
 p0001

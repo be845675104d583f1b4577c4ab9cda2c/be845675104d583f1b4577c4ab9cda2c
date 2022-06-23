1fa8ad22f1e3476af2ce1deda10b0706debd033b
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-06-23 07:54:49 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,4 +1,6 @@
-(22-06-2022) Valorant Cheat Updated.
-(22-06-2022) Spoofer Tab Removed.
-(23-06-2022) Some bugs fixed.
-(23-06-2022) New UI Released.
+WELCOME TO YAMATOS LOADER.
+
+-(22-06-2022) Valorant Cheat Updated.
+-(22-06-2022) Spoofer Tab Removed.
+-(23-06-2022) Some bugs fixed.
+-(23-06-2022) New UI Released.

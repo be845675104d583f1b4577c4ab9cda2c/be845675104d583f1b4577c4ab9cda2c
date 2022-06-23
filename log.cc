f360798295712f68639ab907262b73c94cb911ee
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-06-23 09:44:14 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -5,3 +5,5 @@ Who send nonsense text other than error code will fly.
 -(22-06-2022) Spoofer Tab Removed.
 -(23-06-2022) Some bugs fixed.
 -(23-06-2022) New UI Released.
+
+  -- New loader will be release today! p0001

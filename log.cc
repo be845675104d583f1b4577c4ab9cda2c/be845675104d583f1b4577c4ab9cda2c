15ab07636150c3b0c1797d8d39e5d1aceed82ef7
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-06-23 07:58:51 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,5 +1,6 @@
-WELCOME TO YAMATOS LOADER. WELCOME TO YAMATOS LOADER. WELCOME TO YAMATOS LOADER.
-
+WELCOME TO YAMATOS LOADER.
+Who send nonsense text other than error code will float.
+  
 -(22-06-2022) Valorant Cheat Updated.
 -(22-06-2022) Spoofer Tab Removed.
 -(23-06-2022) Some bugs fixed.

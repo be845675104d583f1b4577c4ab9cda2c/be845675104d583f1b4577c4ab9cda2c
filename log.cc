aa98c53b480b9719e7b99ea3d6ac103522e908f6
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-07-09 21:35:29 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,20 +1,20 @@
-Welcome to Yamatos Loader.
+ Welcome to Yamatos Loader.
   
  # Announcement #
--(09-07-2022) Valorant Cheat is under maintenance. It will open in a few hours!
+ -(09-07-2022) Valorant Cheat is under maintenance. It will open in a few hours!
   
  # Loader #
--(22-06-2022) Spoofer Tab Removed.
--(23-06-2022) Some bugs fixed.
--(23-06-2022) New UI Released. (removed)
--(24-06-2022) Spoofer BSOD issue solved.
--(24-06-2022) Auto Update [NEW]
--(09-07-2022) Old UI Released.
+ -(22-06-2022) Spoofer Tab Removed.
+ -(23-06-2022) Some bugs fixed.
+ -(23-06-2022) New UI Released. (removed)
+ -(24-06-2022) Spoofer BSOD issue solved.
+ -(24-06-2022) Auto Update [NEW]
+ -(09-07-2022) Old UI Released.
   
  # Cheat #
--(09-07-2022) Valorant Cheat Updated.
--(09-07-2022) Sometimes crash problem solved in some windows versions.
--(09-07-2022) Visible function improved.
--(09-07-2022) Aimbot improved. (tested=superud1337)
+ -(09-07-2022) Valorant Cheat Updated.
+ -(09-07-2022) Sometimes crash problem solved in some windows versions.
+ -(09-07-2022) Visible function improved.
+ -(09-07-2022) Aimbot improved. (tested=superud1337)
   
 p0001

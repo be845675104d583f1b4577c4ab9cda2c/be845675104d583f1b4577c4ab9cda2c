30649a19bdcc2584dc48d6cac260297c473e6ae1
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-07-09 21:32:50 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,11 +1,19 @@
-WELCOME TO YAMATOS LOADER.
-
+Welcome to Yamatos Loader.
+  
+ # Announcement #
+-(09-07-2022) Valorant Cheat is under maintenance. It will open in a few hours!
+  
+ # Loader #
 -(22-06-2022) Spoofer Tab Removed.
 -(23-06-2022) Some bugs fixed.
--(23-06-2022) New UI Released.
+-(23-06-2022) New UI Released. (removed)
 -(24-06-2022) Spoofer BSOD issue solved.
 -(24-06-2022) Auto Update [NEW]
+-(09-07-2022) Old UI Released.
+  
+ # Cheat #
+-(09-07-2022) Valorant Cheat Updated.
+-(09-07-2022) Sometimes crash problem solved in some windows versions.
+-(09-07-2022) Visible function improved.
 
--(01-07-2022) Valorant Cheat Updated.
--(09-07-2022) Valorant Cheat is under maintenance. It will open in a few hours!
 p0001

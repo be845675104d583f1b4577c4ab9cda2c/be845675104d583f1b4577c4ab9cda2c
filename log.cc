5c83f128e841c60d59b57d24d41b8ed67e3a54e4
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-07-12 18:03:57 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,18 +1,2 @@
  Welcome to Yamatos Loader.
-  
- # Announcement #
- -(09-07-2022) -
-  
- # Loader #
- -(22-06-2022) Spoofer Tab Removed.
- -(23-06-2022) Some bugs fixed.
- -(23-06-2022) New UI Released. (removed)
- -(24-06-2022) Spoofer BSOD issue solved.
- -(24-06-2022) Auto Update [NEW]
- -(09-07-2022) Old UI Released.
-  
- # Cheat #
- -(09-07-2022) Valorant Cheat Updated.
- -(09-07-2022) Sometimes crash problem solved in some windows versions.
- -(09-07-2022) Visible function improved.
- -(09-07-2022) Aimbot improved. (tested=superud1337)
+ • There will be no more sales. The cheat will active until all licenses expire.

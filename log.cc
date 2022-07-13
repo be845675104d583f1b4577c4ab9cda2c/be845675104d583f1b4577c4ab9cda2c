54db6da99c7a695e3e0fe15a7c2fc4d6a95e0c5c
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-07-14 00:43:59 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -15,4 +15,3 @@ Welcome to Yamatos Loader.
  -(09-07-2022) Sometimes crash problem solved in some windows versions.
  -(09-07-2022) Visible function improved.
  -(09-07-2022) Aimbot improved. (tested=ud)
- -(09-07-2022) Valorant Cheat Updated.

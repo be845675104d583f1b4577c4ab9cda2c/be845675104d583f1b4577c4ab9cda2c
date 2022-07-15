08a2a8399dcd63871cc600443895382ac95cd5d4
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-07-15 06:29:45 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -14,4 +14,4 @@ Welcome to Yamatos Loader.
  # Cheat #
  -(09-07-2022) Sometimes crash problem solved in some windows versions.
  -(09-07-2022) Visible function improved.
- -(09-07-2022) Aimbot improved. (tested=ud)
+ -(09-07-2022) Aimbot improved. (tested=ud)p0001

8dbb6e1173d0fb45cfa947487b254921b806dbaa
be845675104d583f1b4577c4ab9cda2c <94849335+be845675104d583f1b4577c4ab9cda2c@users.noreply.github.com>
2022-07-15 06:29:28 +0300

    Update log.cc

--- log.cc
+++ log.cc
@@ -1,7 +1,7 @@
 Welcome to Yamatos Loader.
 
  # Announcement #
- -(14-07-2022) Valorant Cheat Updated.
+ -(15-07-2022) YamatosCheat will be maintance for a few days. (2-3 days)
  
  # Loader #
  -(22-06-2022) Spoofer Tab Removed.
